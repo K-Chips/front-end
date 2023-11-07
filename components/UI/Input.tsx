0e823e5d4877445a73a59b5241e9afbe4e153887
minjee <minjeeson.dev@gmail.com>
2023-11-07 15:10:50 +0000

    chore: add outline-none to Input component

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -5,7 +5,12 @@ import { cn } from '@/utils/cn';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = ({ className, ...props }: InputProps) => {
-  return <input className={cn('py-[16px] px-[24px]', className)} {...props} />;
+  return (
+    <input
+      className={cn('py-[16px] px-[24px] outline-none', className)}
+      {...props}
+    />
+  );
 };
 
 export default Input;
